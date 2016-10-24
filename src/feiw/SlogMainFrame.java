06b6a7e0bd624c423f33cd16b0d378718b0ece05
Fei Wang <fei.wang@gm.com>
2016-10-24 14:40:07 -0400

    Add swt-linux64.jar

--- src/feiw/SlogMainFrame.java
+++ src/feiw/SlogMainFrame.java
@@ -500,7 +500,7 @@ public final class SlogMainFrame {
             public void widgetSelected(SelectionEvent e) {
                 MessageBox m = new MessageBox(getShell(), SWT.OK | SWT.ICON_INFORMATION);
                 m.setText("About SuperLog");
-                m.setMessage("SuperLog Version 1.0.2\n");
+                m.setMessage("SuperLog Version 1.0.3\n");
                 m.open();
             }
         });
